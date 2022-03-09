3caf9bfb6d1f76b488a3cf606e32b38a65e2a725
세젤귀 고양이들 <46598063+qwreey75@users.noreply.github.com>
2022-03-09 23:06:28 +0900

    Reindented space into tab

--- init.lua
+++ init.lua
@@ -7,54 +7,54 @@ local insert = table.insert;
 
 local unitMut = 1000;
 local units = {
-    {1e3,"us"},{1e6,"ms"},{1e9,"sec"}
+	{1e3,"us"},{1e6,"ms"},{1e9,"sec"}
 };
 
 local profiler = {};
 profiler.__index = profiler;
 
 function profiler.new(projectName)
-    projectName = projectName or "default";
+	projectName = projectName or "default";
 
-    local this = {};
+	local this = {};
 
-    this._project = projectName;
-    setmetatable(this,profiler);
+	this._project = projectName;
+	setmetatable(this,profiler);
 
-    return this;
+	return this;
 end
 
 function profiler:start(descript)
-    local block = self._block;
-    local new = {
-        _descript = descript;
-        _parent = block;
-    };
-    if block then
-        new._root = self._root;
-    else
-        self._root = new;
-    end
-    insert(block or self,new);
-    self._block = new;
-    new._birth = hrtime();
+	local block = self._block;
+	local new = {
+		_descript = descript;
+		_parent = block;
+	};
+	if block then
+		new._root = self._root;
+	else
+		self._root = new;
+	end
+	insert(block or self,new);
+	self._block = new;
+	new._birth = hrtime();
 end
 
 function profiler:stop()
-    local now = hrtime();
+	local now = hrtime();
 
-    local block = self._block;
-    if not block then
-        error"profiler was already stopped";
-    end
-    block._death = now;
-    self._block = block._parent;
+	local block = self._block;
+	if not block then
+		error"profiler was already stopped";
+	end
+	block._death = now;
+	self._block = block._parent;
 end
 
 local floor = math.floor;
 local function point(x,pos)
-    pos = 10^pos;
-    return floor(x*pos)/pos;
+	pos = 10^pos;
+	return floor(x*pos)/pos;
 end
 
 local indent = " |- ";
@@ -65,42 +65,42 @@ local percent = " (Par:%s%%,Rot:%s%%)";
 local rep = string.rep;
 local format = string.format;
 local function output(thing,indentLevel,str)
-    indentLevel = indentLevel or 0;
-    str = str or "";
+	indentLevel = indentLevel or 0;
+	str = str or "";
 
-    local timestamp = thing._death - thing._birth;
-    local unit,timestampFormatted;
+	local timestamp = thing._death - thing._birth;
+	local unit,timestampFormatted;
 
-    for _,unitData in ipairs(units) do
-        timestampFormatted = timestamp / unitData[1];
-        unit = unitData[2];
-        if timestampFormatted < unitMut then break; end
-    end
+	for _,unitData in ipairs(units) do
+		timestampFormatted = timestamp / unitData[1];
+		unit = unitData[2];
+		if timestampFormatted < unitMut then break; end
+	end
 
-    local root,parent = thing._root,thing._parent;
-    thing._timestamp = timestamp;
+	local root,parent = thing._root,thing._parent;
+	thing._timestamp = timestamp;
 
-    str = str .. format(itemFormat,
-        rep(indent,indentLevel),
-        thing._descript,
-        point(timestampFormatted,points),unit,
-        root and parent and format(percent,
-            tostring(point((timestamp/parent._timestamp)*100,percentPoints)),
-            tostring(point((timestamp/root._timestamp  )*100,percentPoints))
-        ) or ""
-    )
+	str = str .. format(itemFormat,
+		rep(indent,indentLevel),
+		thing._descript,
+		point(timestampFormatted,points),unit,
+		root and parent and format(percent,
+			tostring(point((timestamp/parent._timestamp)*100,percentPoints)),
+			tostring(point((timestamp/root._timestamp  )*100,percentPoints))
+		) or ""
+	)
 
-    local nextIndent = indentLevel + 1;
-    for _,child in ipairs(thing) do
-        str = output(child,nextIndent,str);
-    end
+	local nextIndent = indentLevel + 1;
+	for _,child in ipairs(thing) do
+		str = output(child,nextIndent,str);
+	end
 
-    return str;
+	return str;
 end
 
 function profiler:print(index)
-    index = index and (index - 1) or 0
-    return output(self[#self - index]);
+	index = index and (index - 1) or 0
+	return output(self[#self - index]);
 end
 
 return profiler;
