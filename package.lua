788f70e0e85753619a226b91a6e64610fbb6022b
qwreey <me@qwreey.moe>
2023-12-28 00:38:51 +0000

    Fix indent

--- package.lua
+++ package.lua
@@ -2,7 +2,7 @@ return {
 	name = 'qwreey/profiler',
 	version = '2.1.1',
 	homepage = 'https://github.com/qwreey/profiler.lua',
-  description = 'Profiling tasks',
+	description = 'Profiling tasks',
 	dependencies = {},
 	tags = {'performance', 'profiling'},
 	license = 'MIT',
